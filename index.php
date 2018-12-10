5819e635bd6ee19bc8d5180c1edc0a50682619f0
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-12-10 12:31:02 +0100

    jolie

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Just Do It</title>
     <link rel="stylesheet" href="css/style.css">
     <!-- Materialize CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/materialize.css">
 </head>
 <body>
     <div class="row">
